4f99938f916ac1a868e2c979f84adbdaa23e0f46
박성현 <110206842+shPark95@users.noreply.github.com>
2025-05-10 22:29:00 +0900

    Add: domain

---
+++ src/main/java/com/server/calendarapp/domain/schedule/service/ScheduleService.java
@@ -0,0 +1,32 @@
+package com.server.calendarapp.domain.schedule.service;
+
+import com.server.calendarapp.domain.schedule.dto.ScheduleDto;
+import com.server.calendarapp.domain.schedule.model.Schedule;
+import com.server.calendarapp.domain.schedule.repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public void createSchedule(ScheduleDto scheduleDto) {
+        Schedule schedule = new Schedule();
+        schedule.setTitle(scheduleDto.getTitle());
+        schedule.setAuthor(scheduleDto.getAuthor());
+        schedule.setPassword(scheduleDto.getPassword());
+        schedule.setCreatedAt(LocalDateTime.now());
+        schedule.setUpdatedAt(LocalDateTime.now());
+
+        schedule.setMemberId(1L);
+
+        scheduleRepository.save(schedule);
+    }
+
+}
